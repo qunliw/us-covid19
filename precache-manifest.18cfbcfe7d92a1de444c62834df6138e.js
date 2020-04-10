9b9e2426124f6c8e19684f812d7f805b51236fc7
qunliw <62570840+qunliw@users.noreply.github.com>
2020-04-10 04:01:05 -0500

    Updates

---
+++ precache-manifest.18cfbcfe7d92a1de444c62834df6138e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfe9c3f8a497e37bfcdb5fe7daf22e8",
+    "url": "/us-covid19/index.html"
+  },
+  {
+    "revision": "825b1ca437bf1470f06a",
+    "url": "/us-covid19/static/js/2.703174fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/us-covid19/static/js/2.703174fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22474f9daf40f83b568b",
+    "url": "/us-covid19/static/js/main.8bb3245e.chunk.js"
+  },
+  {
+    "revision": "67ad748868654e1ca738",
+    "url": "/us-covid19/static/js/runtime-main.9e0bb3fb.js"
+  }
+]);
